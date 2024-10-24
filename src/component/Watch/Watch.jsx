8ae8f7f87ffe8245775e26434c1bf94e3f0260eb
Your Name <k.bristi057@gmail.com>
2024-10-25 04:49:26 +0600

    added some files

--- src/component/Watch/Watch.jsx
+++ src/component/Watch/Watch.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 
 import './Watch.css'
 
-const Watch = () => {
+const Watch = ({watch}) => {
+    const {name, price}= watch
     return (
         <div>
-            
+            <h4>Watches: {name}</h4>
+            <p>Price: {price}</p>
         </div>
     );
 };
